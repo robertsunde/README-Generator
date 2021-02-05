6ed53afb41289c273e9f040adaaf76190f94e4e1
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-04 22:36:38 -0500

    added questions for prompt

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+inquirer.prompt([
+    {
+        type: "input",
+        name: "applicationtitle",
+        message: "What is the title of your application/project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Enter a brief description about your application.",
+      },
+      {
+        type: "input",
+        name: "tableofcontents",
+        message: "????",
+      },
+      {
+        type: "input",
+        name: "Installation",
+        message: "Input information needed for product install/execution.",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Enter usage details here.",
+      },
+      {
+        type: "input",
+        name: "contributions",
+        message: "Include any contributors/resources here.",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Enter test details here.",
+      },
+    ]);
+
+questions()
+.then((answers) => writeFileAsync('README.md', generateHTML(answers)))
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeFile('README.md', data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
