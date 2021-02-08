b2c307419c1f02559117221611bc5185b24e73b3
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-02-07 21:24:55 -0500

    fixed display issue, now testing

--- index.js
+++ index.js
@@ -524,13 +524,6 @@ inquirer.prompt([
     message: "Enter contact email here.",
   },
 
-  // Application photo here.
-  {
-    type: "input",
-    name: "photo",
-    message: "Please paste a read-me friendly application photo link here.",
-  },
-
   // License type here.
   {
     type: "list",
@@ -551,7 +544,6 @@ inquirer.prompt([
 const READMECreator = (answers) => {
   return `
 # ${answers.applicationtitle} <br/>
-${answers.photo} <br/>
 ${licenseBadge(answers.license)} <br/>
 <br/>
 <br/>
